5f3f5583b946f75883728a00ba6d36e7d5e564c0
slyhLff <c1471373444@outlook.com>
2025-11-04 17:55:32 +0800

    重构 2.py 文件，优化聊天助手功能并调整代码结构

--- 2.py
+++ 2.py
@@ -1,6 +1,22 @@
-from langchain_core.prompts import ChatPromptTemplate
-
 # 方式一
 # LangChain为各llm提供了统一的接口：ChatModel
-from langchain_deepseek.chat_model import ChatModel
+from langchain_deepseek.chat_models import ChatDeepSeek
 
+llm = ChatDeepSeek(model="deepseek-chat", api_key="sk-4d60ba5196d14f939126d5e3b5f1647a")
+
+response = llm.invoke("给我讲个关于量子纠缠的笑话")
+print(response.content)
+
+# 方式二
+# 通过链式结构
+from langchain_core.prompts import ChatPromptTemplate
+
+prompt = ChatPromptTemplate.from_template("给我讲个关于{topic}的笑话")
+
+model = ChatDeepSeek(
+    model="deepseek-chat", api_key="sk-4d60ba5196d14f939126d5e3b5f1647a"
+)
+chain = prompt | model
+
+response = chain.invoke({"topic": "量子纠缠"})
+print(response.content)
