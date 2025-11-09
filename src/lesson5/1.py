81d280428fa3a5a536e77dfe73a1d113b6b1daf3
William Lao <c1471373444@outlook.com>
2025-11-09 23:17:01 +0800

    n

--- src/lesson5/1.py
+++ src/lesson5/1.py
@@ -1,1 +1,16 @@
-from langchain_deepseek import 
+import requests
+
+url = "https://api.siliconflow.cn/v1/embeddings"
+
+payload = {
+    "model": "BAAI/bge-m3",
+    "input": "Silicon flow embedding online: fast, affordable, and high-quality embedding services. come try it out!"
+}
+headers = {
+    "Authorization": "Bearer sk-ljuovztdpgqafjnufoxhqbuzfzyvctualkzjxekycvpywxpx",
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.json())
