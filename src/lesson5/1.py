1c6034bb6ebf46dad78b7346618242208b25bf69
slyhLff <c1471373444@outlook.com>
2025-11-10 18:18:05 +0800

    Refactor code structure for improved readability and maintainability

--- src/lesson5/1.py
+++ src/lesson5/1.py
@@ -1,16 +1,23 @@
-import requests
+import os
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.utils.math import cosine_similarity
 
-url = "https://api.siliconflow.cn/v1/embeddings"
+os.environ["OPENAI_API_KEY"] = "sk-ljuovztdpgqafjnufoxhqbuzfzyvctualkzjxekycvpywxpx"
 
-payload = {
-    "model": "BAAI/bge-m3",
-    "input": "Silicon flow embedding online: fast, affordable, and high-quality embedding services. come try it out!"
-}
-headers = {
-    "Authorization": "Bearer sk-ljuovztdpgqafjnufoxhqbuzfzyvctualkzjxekycvpywxpx",
-    "Content-Type": "application/json"
-}
+emb = OpenAIEmbeddings(model="BAAI/bge-m3", base_url="https://api.siliconflow.cn/v1")
 
-response = requests.post(url, json=payload, headers=headers)
+# 嵌入查询和文档
+q_vec = emb.embed_query("query:NLP")
+d_vecs = emb.embed_documents(
+    [
+        "passage:使用BGE并进行向量归一化有帮助",
+        "passage:深度学习模型在NLP中很流行",
+        "passage:向量数据库用于高效检索",
+    ]
+)
 
-print(response.json())
+print(f"查询向量维度: {len(q_vec)}")
+print(f"文档数量: {len(d_vecs)}, 每个文档向量维度: {len(d_vecs[0])}")
+
+scores=cosine_similarity([q_vec],d_vecs)[0]
+print(scores)
